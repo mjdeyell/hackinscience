003d0454ea28e08db085d53f1409a97da1afdbba
Matt <mjdeyell@gmail.com>
2015-09-21 17:44:55 +0200

    ex 90

---
+++ exercises/090/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+import sys
+file = sys.argv
+l = list(enumerate(file))
+for i in range(len(l)):
+    temp = list[i]
+    print(str(temp[0]) + " " + temp[1])
