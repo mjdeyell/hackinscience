ba2780bee47c6e7a93bddbcac9e17b42761e4db5
Matt <mjdeyell@gmail.com>
2015-09-21 12:55:26 +0200

    fixed 19

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -7,7 +7,9 @@ Created on Mon Sep 21 10:38:13 2015
 import sys
 file = sys.argv
 if len(file) > 2:
-    Answer = file(1) + file(2)
+    A = file[1]
+    B = file[2]
+    Answer = int(A) + int(B)
     print(Answer)
 else:
     print("usage: python3 solution.py OP1 OP2")
