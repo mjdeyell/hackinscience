b2e0c573ab5dc5093a2a77f85d8861e4ec91b1b9
Matt <mjdeyell@gmail.com>
2015-09-21 14:57:04 +0200

    changed time to exclude microseconds

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -5,6 +5,9 @@ Created on Mon Sep 21 10:38:13 2015
 @author: MDeyell
 """
 import datetime
-A = datetime.date.today()
-C = datetime.datetime.now().time()
-print("Today is " + str(A) + " and it is " + str(C))
+fmt1 = '%Y %m %d'
+fmt2 = '%H:%M:%S'
+C = datetime.datetime.now()
+A = C.strftime(fmt1)
+B = C.strftime(fmt2)
+print("Today is " + str(A) + " and it is " + str(B))
