d003ef1000cbee19ea4f19feba44a1ecf4108a51
Matt <mjdeyell@gmail.com>
2015-09-21 14:41:05 +0200

    added 25

---
+++ exercises/025/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+import datetime
+A = datetime.date.today()
+C = datetime.datetime.now().time()
+print("Today is " + str(A) + " and it is " + str(C))
