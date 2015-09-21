2f3a114fcb81f7f8f0a8582bfd3042e38640b89d
Matt <mjdeyell@gmail.com>
2015-09-21 18:18:11 +0200

    removed some lists

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -7,11 +7,11 @@ Created on Mon Sep 21 10:38:13 2015
 
 
 def love_meet(input1, input2):
-    outvar = list(set(input1).intersection(input2))
+    outvar = set(input1).intersection(input2)
     return outvar
 
 
 def affair_meet(input1, input2, input3):
     meet = list(set(input1).intersection(input3))
-    avoid = list(set(meet).difference(input2))
+    avoid = set(meet).difference(input2)
     return avoid
