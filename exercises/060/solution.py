83dfeffea542e10899bf06f03ac58f866fcc1d1d
Matt <mjdeyell@gmail.com>
2015-09-21 15:40:10 +0200

    double letters

---
+++ exercises/060/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+blah = "abcdefghijklmnopqrstuvwxyz"
+for i in range(len(blah)):
+    A = blah[i]
+    for j in range(len(blah)):
+        B = blah[j]
+        print(A+B)
