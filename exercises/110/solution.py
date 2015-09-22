5fa5c2a8d88dc2103671312e44989774ef090c93
Matt <mjdeyell@gmail.com>
2015-09-22 13:47:45 +0200

    stopped script from continuing even with error

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -11,23 +11,24 @@ if (len(sys.argv) == 4):
         num1 = int(sys.argv[1])
         num2 = int(sys.argv[3])
         op = str(sys.argv[2])
+        operators = ['+', '-', '*', '/', '%', '^']
+        if (op == '+'):
+            print(num1 + num2)
+        elif (op == '-'):
+            print(num1 - num2)
+        elif (op == '*'):
+            print(num1 * num2)
+        elif (op == '/'):
+            print(num1 / num2)
+        elif (op == '%'):
+            print(num1 % num2)
+        elif (op == '^'):
+            print(num1 ** num2)
+        else:
+            print('input error')
     except ValueError:
         print('input error')
 
-    operators = ['+', '-', '*', '/', '%', '^']
-    if (op == '+'):
-        print(num1 + num2)
-    elif (op == '-'):
-        print(num1 - num2)
-    elif (op == '*'):
-        print(num1 * num2)
-    elif (op == '/'):
-        print(num1 / num2)
-    elif (op == '%'):
-        print(num1 % num2)
-    elif (op == '^'):
-        print(num1 ** num2)
-    else:
-        print('input error')
+
 else:
     print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')
