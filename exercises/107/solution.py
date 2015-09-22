af9a6b12fab5c63f971720216d474d79a8f386f8
Matt <mjdeyell@gmail.com>
2015-09-22 12:48:31 +0200

    107 test run

---
+++ exercises/107/solution.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+
+
+def select_student(student_list, grade_threshold):
+    sl = list(student_list)
+    gt = int(grade_threshold)
+    good = []
+    bad = []
+    
+    
+    def getKey(item):
+        return item[1]
+    
+    
+    for i in sl:
+        st = list(sl[i])
+        if (st[1] >= gt):
+            good.append(st)
+        elif (st[1] < gt):
+            bad.append(st)
+            
+    sort_good = sorted(good, key=getKey, reverse=True)
+    sort_bad = sorted(bad, key=getKey)
+    
+    return {'Accepted': sort_good
+            'Rejected': sort_bad
+            }
