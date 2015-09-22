e341450ae18d462227e11445f28a8ce2dfde32f6
Matt <mjdeyell@gmail.com>
2015-09-22 10:51:25 +0200

    more pep8 bullshit

--- exercises/100/solution.py
+++ exercises/100/solution.py
@@ -5,11 +5,11 @@ Created on Mon Sep 21 10:38:13 2015
 @author: MDeyell
 """
 station = {
- 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
- 'number': 31705,
- 'latitude': 48.8645278209514,
- 'name': 'CHAMPEAUX (BAGNOLET)',
- 'longitude': 2.416170724425901
+   'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+   'number': 31705,
+   'latitude': 48.8645278209514,
+   'name': 'CHAMPEAUX (BAGNOLET)',
+   'longitude': 2.416170724425901
 }
 
 for k, v in station.items():
