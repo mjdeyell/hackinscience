fb5734e4036ad1b88402f23b345c785e73f772fa
Matt <mjdeyell@gmail.com>
2015-09-22 14:55:39 +0200

    added idiot protection

--- exercises/202/solution.py
+++ exercises/202/solution.py
@@ -9,12 +9,15 @@ Created on Mon Sep 21 10:38:13 2015
 def starts_with(haystack, needle):
     starts = [0] * len(needle)
     out = True
-    for i in range(len(needle)):
-        if (haystack[i] == needle[i]):
-            starts[i] = 1
-        else:
-            starts[i] = 0
-    for i in range(len(starts)):
-        if (starts[i] == 0):
-            out = False
+    if (len(haystack) >= len(needle)):
+        for i in range(len(needle)):
+            if (haystack[i] == needle[i]):
+                starts[i] = 1
+            else:
+                starts[i] = 0
+        for i in range(len(starts)):
+            if (starts[i] == 0):
+                out = False
+    else:
+        out = False
     return out
