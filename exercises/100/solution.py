6af581b9cc9a32f9c6ae3d5f8b929014fb69ac95
Matt <mjdeyell@gmail.com>
2015-09-22 10:47:49 +0200

    ex 100

---
+++ exercises/100/solution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+
+for k, v in station.items():
+    print(k, v)
