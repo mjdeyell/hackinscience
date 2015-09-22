4dbfa4d3ddbee9ddbca9dd7beb7adaeac6a4d7fb
Matt <mjdeyell@gmail.com>
2015-09-22 14:09:46 +0200

    support for numbers < 2

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -9,13 +9,16 @@ Created on Mon Sep 21 10:38:13 2015
 def is_prime(number):
     testset = range(1, int(number**0.5)+1)
     prime = 0
-    for i in range(len(testset)):
-        p = number % testset[i]
-        if (p == 0):
-            prime = prime + 1
-    if (prime == 1):
-        return 'True'
-    elif (prime > 1):
-        return 'False'
+    if (number > 1):
+        for i in range(len(testset)):
+            p = number % testset[i]
+            if (p == 0):
+                prime = prime + 1
+        if (prime == 1):
+            return 'True'
+        elif (prime > 1):
+            return 'False'
+        else:
+            return 'Error'
     else:
-        return 'Error'
+        return 'False'
