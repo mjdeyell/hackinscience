10b1cda89301c7e1631c8d31f80bc05540f27cf5
Matt <mjdeyell@gmail.com>
2015-09-22 16:39:20 +0200

    ex 210

---
+++ exercises/210/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+import prime
+
+prime_sum = 0
+for i in range(1000):
+    if (prime.is_prime(i) == True):
+        prime_sum = prime_sum + i
+print(prime_sum)
