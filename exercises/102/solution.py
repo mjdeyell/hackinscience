1e1661cfecf50859af3e73066437eb85d756c0dd
Matt <mjdeyell@gmail.com>
2015-09-22 11:50:46 +0200

    i hate coding

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -6,7 +6,7 @@ Created on Mon Sep 21 10:38:13 2015
 """
 
 
-def check_my_city(city):
+def check_my_city(invar):
     city = invar + '-'
     velib = \
         [{'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE)\
