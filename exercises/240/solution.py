c4e33881d92d6601ab387196671cc11d9bd35bca
Matt <mjdeyell@gmail.com>
2015-09-23 11:04:24 +0200

    ex 240

---
+++ exercises/240/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+n = 10
+output = [1, 2]
+for i in range(2, n):
+    x = output[i-2] + output[i-1]
+    output.append(x)
+print(", ".join(str(e) for e in out))
