63374d69833d95a417fd814dfa8b448eb72dfe37
Matt <mjdeyell@gmail.com>
2015-09-23 16:08:49 +0200

    ex 330 doesn't work

---
+++ exercises/330/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:38:13 2015
+
+@author: MDeyell
+"""
+import json
+
+
+def load_jason(path):
+    with open(path) as jayson:
+        out = json.load(jayson)
+    return out
+
