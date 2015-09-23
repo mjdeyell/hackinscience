0e191587d6d8e570b124247c193c65094653ad85
Matt <mjdeyell@gmail.com>
2015-09-23 15:06:02 +0200

    ex 320

--- exercises/310/solution.py
+++ exercises/310/solution.py
@@ -7,8 +7,16 @@ Created on Mon Sep 21 10:38:13 2015
 
 f = open('words.txt', 'r')
 a = f.read()
-count = 0
+b = {}
+
 for i in range(len(a)):
-    if (a[i] == 'e'):
-        count = count + 1
-print(count)
+    try:
+        b[a[i]] = b[a[i]] + 1
+    except Exception:
+        b[a[i]] = 1
+
+del b['\n']
+
+for k in b:
+    c = b[k]/len(a)
+    print(k + ':', "%.2f" % round(c, 2))
