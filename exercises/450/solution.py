c804031457e055b40972ab9b47a264fe7b730c0c
Matt <mjdeyell@gmail.com>
2015-09-24 13:44:45 +0200

    changed range to 101 to include 100

--- exercises/450/solution.py
+++ exercises/450/solution.py
@@ -4,26 +4,16 @@ Created on Mon Sep 21 10:38:13 2015
 
 @author: MDeyell
 """
+from string import ascii_lowercase
+forward = 1
+backward = -1
 
 
-def filtered(items, lambda_expression):
-    out = []
-    for i in range(len(items)):
-        if lambda_expression(items[i]):
-            out.append(items[i])
-    return out
-
-
-# items = [1, 2, 3, 4, 5, 6]
-# even = filtered(items, lambda x: x % 2 == 0)
-# odd = filtered(items, lambda x: x % 2 == 1)
-# print(even)
-# print(odd)
-
-if __name__ == '__main__':
-    x1 = filtered(range(1, 100), lambda x: x % 3 == 0)
-    print(', '.join(str(e) for e in x1))
-    x2 = filtered(range(1, 100), lambda x: x % 5 == 0)
-    print(', '.join(str(e) for e in x2))
-    x3 = filtered(range(1, 100), lambda x: x % 15 == 0)
-    print(', '.join(str(e) for e in x3))
+def caeser_cypher(s, key, method):
+    try:
+        output_string = []
+        input_string = s.split()
+        if (key < 26):
+            for i in range(len(input_string)):
+                for k in range(len(input_string[i])):
+                    output
