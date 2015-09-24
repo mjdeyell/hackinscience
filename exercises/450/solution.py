c8d13ba45f81d68a5f7692025816da4c41898cef
Matt <mjdeyell@gmail.com>
2015-09-24 14:15:41 +0200

    pep8 error

--- exercises/450/solution.py
+++ exercises/450/solution.py
@@ -7,7 +7,7 @@ Created on Mon Sep 21 10:38:13 2015
 from string import ascii_lowercase
 forward = 1
 backward = -1
-double_ascii = ascii_lowercase + ascii_lowercase
+d_ascii = ascii_lowercase + ascii_lowercase
 
 
 def caeser_cypher(s, key, method):
@@ -21,18 +21,18 @@ def caeser_cypher(s, key, method):
                 for k in range(len(input_string[i])):
                     for l in range(len(ascii_lowercase)):
                         if (input_string[i][k] == ascii_lowercase[l]):
-                            coded_word = coded_word +\
-                                        double_ascii[l + (key * method)]
+                            coded_word = (coded_word +
+                                          d_ascii[l + (key * method)])
                 output_string.append(coded_word)
         elif (key > 26):
-            lower_key = key % 26
+            l_key = key % 26
             for i in range(len(input_string)):
                 coded_word = str()
                 for k in range(len(input_string[i])):
                     for l in range(len(ascii_lowercase)):
                         if (input_string[i][k] == ascii_lowercase[l]):
-                            coded_word = coded_word +\
-                                        double_ascii[l + (lower_key * method)]
+                            coded_word = (coded_word +
+                                          d_ascii[l + (l_key * method)])
                 output_string.append(coded_word)
         out_put = ' '.join(str(e) for e in output_string)
         return out_put
